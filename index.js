d0eaa412c5dde67888fcacc27db3ee1f271f591e
FFefo <felpo.correiacosta@gmail.com>
2024-08-02 18:20:26 -0300

    ok

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express"
 const servidor = express()
 
-servidor.listen(5001, () => console.log('A API ta no galera'))
 
 servidor.get('/calculadora/soma/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
     let soma = n1 + n2  
-
+    
     resp.send('A soma dos números é: ' + soma);
 })
+
+
+
+servidor.listen(5001, () => console.log('A API ta no galera'))
