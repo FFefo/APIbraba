828880fd436c3c296b7d856af0f6ba05dadd006c
Felipe Correia Costa <ra51415081883@cursos.insf>
2024-08-02 18:01:31 -0300

    função somar criada

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+const servidor = express()
+
+servidor.get('/calculadora/soma/:n1/:n2', (req, resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+    let soma = n1 + n2  
+
+    resp.send('A soma dos dois números é: ' + soma);
+})
+
+servidor.listen(5001, () => console.log('A API ta no galera'))
